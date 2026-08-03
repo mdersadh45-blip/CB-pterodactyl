0ca5a3fc0c61912200737cd06bdd8879e3120a63
mdersadh45-blip <mdersadh45@gmail.com>
2026-08-03 11:03:54 +0530

    Update and rename puffer-panel.txt to PUFFER/puffer-panel.sh

--- PUFFER/puffer-panel.sh
+++ PUFFER/puffer-panel.sh
@@ -6,3 +6,5 @@ apt install systemctl
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh?any=true | sudo bash
 sudo apt update
 sudo apt-get install pufferpanel
+sudo pufferpanel user add
+sudo systemctl enable --now pufferpanel
